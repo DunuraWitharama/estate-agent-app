d65947f231c1319166666a0f305a974d48da83a4
Dunura Witharama <dunurawitharama@gmail.com>
2026-01-04 20:32:16 +0530

    Switch to HashRouter and fix base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// 1. CHANGED: Import HashRouter (aliased as Router so we don't break your existing code tags)
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { FavouritesProvider } from './context/FavouritesContext';
 import SearchPage from './pages/SearchPage';
 import PropertyPage from './pages/PropertyPage';
@@ -8,6 +9,7 @@ import './styles.css';
 function App() {
   return (
     <FavouritesProvider>
+      {/* 2. This <Router> is now actually a HashRouter */}
       <Router>
         <div className="app">
           <header className="main-header">
